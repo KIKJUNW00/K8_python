bb72d397184120aa52f17288d8e8aee7b03d5b33
KIKJUNW00 <kimjun9758@naver.com>
2024-09-20 14:32:19 +0900

    practice

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+# print("hello world!")
+
+# message = "Hello world!"
+# print(message)
+
+# message = "OMG Hello world!"
+# print(message)
+
+# name = "ada lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+message = f"Hello, {full_name}!"
+print(message)
+
